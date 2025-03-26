ecba6f2c3586f49173f0042b0f57e42b8747f3ac
Ramon Mello <ramonomello@gmail.com>
2025-03-26 22:36:28 +0000

    feat: add simple default footer

--- components/footer/defaultFooter.tsx
+++ components/footer/defaultFooter.tsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
+
 export function DefaultFooter() {
   return (
-    <footer>
-      <p>Default Footer</p>
+    <footer className="flex self-end w-full">
+      <div className="flex ml-auto">
+        <a
+          className="flex mr-4"
+          href="https://github.com/ramonmello"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            className="text-amber-200 fill-amber-200"
+            src="/github-icon.svg"
+            alt="File icon"
+            width={24}
+            height={24}
+          />
+        </a>
+        <a
+          className="flex"
+          href="https://www.linkedin.com/in/ramonmello/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/linkedin-icon.svg"
+            alt="File icon"
+            width={24}
+            height={24}
+          />
+        </a>
+      </div>
     </footer>
   );
 }
