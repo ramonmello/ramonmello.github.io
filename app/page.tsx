e71b6384c3aec03a1722d9d7225391fbd610103f
Ramon Mello <ramonomello@gmail.com>
2025-04-27 15:44:36 +0100

    chore: update libs

--- app/page.tsx
+++ app/page.tsx
@@ -33,13 +33,13 @@ export default function Home() {
 
   return (
     <>
-      <div className="absolute  bottom-14 left-1/2 -translate-x-1/2 font-lores-12">
+      <div className="absolute bottom-24  md:bottom-14 left-1/2 -translate-x-1/2 font-lores-12">
         <div className="hidden md:block">
           <span className="font-nimbus">← →</span> : Girar |{" "}
           <span className="font-nimbus">↑</span> : Acelerar |{" "}
           <span className="font-nimbus">Espaço</span> : Disparar
         </div>
-        <div className="md:hidden">Abra no desktop para controlar a nave!</div>
+        <div className="md:hidden w-52">Acesse pelo PC para jogar!</div>
       </div>
       <div className="flex flex-col w-full justify-between relative z-10">
         <canvas
