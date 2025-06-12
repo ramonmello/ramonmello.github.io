9bac8677b9736cf68d2502484ce4b8b1789163eb
Ramon Mello <ramonomello@gmail.com>
2025-06-12 19:35:10 +0100

    feat: integrate Next.js bundle analyzer for performance optimization and add build analysis script

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -11,4 +12,7 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: true,
+})(nextConfig);
