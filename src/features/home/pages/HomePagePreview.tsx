4bc8124b546822bdb7bde92dd4d45b2bdbeb1721
Ramon Mello <ramonomello@gmail.com>
2025-06-15 23:13:52 +0100

    refactor: integrate TinaCMS fields into HomePage and HomePagePreview for enhanced content management

--- src/features/home/pages/HomePagePreview.tsx
+++ src/features/home/pages/HomePagePreview.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { HomePage } from "./HomePage";
-import { mapHome } from "../model/mapper";
 import { HomeCMS } from "@/src/libs/cms/types";
 import { useTina } from "tinacms/dist/react";
 
@@ -12,7 +11,5 @@ export default function HomePagePreview(props: HomeCMS) {
     data: props.data,
   });
 
-  const vm = mapHome(data);
-
-  return <HomePage {...vm} />;
+  return <HomePage {...data.page} />;
 }
